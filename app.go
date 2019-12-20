ad3a4a58f05bc8dd6f66c20262df2c2ec8e1da19
chaixb <chai_xb@163.com>
2019-12-20 10:04:27 +0800

    simple command

--- app.go
+++ app.go
@@ -5,16 +5,17 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func run(args ...string) {
-	add := exec.Command("git", args...)
-	add.Stdout = os.Stdout
-	add.Stderr = os.Stderr
-	add.Run()
+	git := exec.Command("trans", args...)
+	git.Stdout = os.Stdout
+	git.Stderr = os.Stderr
+	git.Run()
 }
 
 func main() {
@@ -22,12 +23,15 @@ func main() {
 
 	switch os.Args[1] {
 	case "zh":
+		args := os.Args[2:]
+		run(strings.Join(args, " "))
 		dictCmd.Parse(os.Args[2:])
-		println("zh")
 	case "en":
+		args := os.Args[2:]
+		run(strings.Join(args, " "))
 		dictCmd.Parse(os.Args[2:])
-		println("en")
 	}
+
 	// db
 	db, err := sqlx.Connect("sqlite3", "dict.db")
 	if err != nil {
